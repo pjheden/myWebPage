816892bab2944b37477c5212a9e750e3169e09ea
pjheden <julianheden@gmail.com>
2017-10-15 13:45:52 +0200

    Add template component

---
+++ src/components/TemplateComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img src="../assets/todo.png" alt="Work in progress">
+
+  </div>
+</template>
+
+<script >
+  export default {
+    name: 'Name (with capital letter)'
+  }
+</script>
+
+<style scoped >
+  img {
+    width: 30em;
+    height: 30em;
+  }
+</style>
